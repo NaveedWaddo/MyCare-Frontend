0676b9bbda3493922b7ecec4b2088a84e4d1061d
Naveed Waddo <naveedwaddo1123@gmail.com>
2025-05-08 20:58:48 +0530

    setup theme and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,51 @@
-const App = () => {
-  return <div className="text-red-500">App</div>;
-};
+import "@mantine/core/styles.css";
 
-export default App;
+import { Button, MantineProvider, createTheme } from "@mantine/core";
+
+const theme = createTheme({
+  focusRing: "never",
+  colors: {
+    primary: [
+      "#f1fcfa",
+      "#cff8ef",
+      "#9ff0e1",
+      "#67e1cf",
+      "#32b9a9",
+      "#1fad9f",
+      "#168b82",
+      "#166f69",
+      "#165955",
+      "#174a47",
+      "#072c2b",
+    ],
+    neutral: [
+      "#f6f6f6",
+      "#e7e7e7",
+      "#d1d1d1",
+      "#b0b0b0",
+      "#888888",
+      "#6d6d6d",
+      "#5d5d5d",
+      "#4f4f4f",
+      "#454545",
+      "#3d3d3d",
+      "#000000",
+    ],
+  },
+
+  primaryColor: "primary",
+  primaryShade: 4,
+  defaultGradient: {
+    from: "primary.4",
+    to: "primary.8",
+    deg: 132,
+  },
+});
+
+export default function App() {
+  return (
+    <MantineProvider theme={theme}>
+      <Button variant="filled">Hello</Button>
+    </MantineProvider>
+  );
+}
